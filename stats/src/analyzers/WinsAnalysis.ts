6c972952b0a03c5a28b7f331afb9700544535968
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-12-07 09:21:22 -0800

    modified match enum

--- stats/src/analyzers/WinsAnalysis.ts
+++ stats/src/analyzers/WinsAnalysis.ts
@@ -1,6 +1,6 @@
 import { Analyzer } from '../Summary';
 import { MatchData } from '../MatchData';
-import { MatchResult } from './MatchResult';
+import { MatchResult } from '../MatchResult';
 
 export class WinsAnalysis implements Analyzer {
   constructor(public team: string) {}

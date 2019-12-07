7b3f455e92d45a6668f787caadb32e0c10d26473
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-12-07 12:27:22 -0800

    started user class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on('change', () => {
-  console.log('cahnge 1');
-});
-user.on('change', () => {
-  console.log('change 2');
-});
-
-user.trigger('change');
+user.save();
